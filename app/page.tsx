91704f15d12ded35745e0f6cc9d408339a1a329b
CHRISTOPHER GOLD <cgoldaz@gmail.com>
2024-03-10 06:14:45 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,134 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react';
+import { TempleWallet } from '@temple-wallet/dapp';
+import { TezosToolkit } from '@taquito/taquito';
+import { InMemorySigner } from '@taquito/signer';
 
-export default function Home() {
+import 'tailwindcss/tailwind.css';
+
+const TezosContract: React.FC = () => {
+  const [connected, setConnected] = useState<boolean>(false);
+  const [walletAddress, setWalletAddress] = useState<string | undefined>('');
+  const [balance, setBalance] = useState<number>();
+  const [tezos, setTezos] = useState<TezosToolkit>();
+  const [Tezos] = useState<TezosToolkit>(new TezosToolkit("https://ghostnet.smartpy.io"));
+
+  Tezos.setProvider({
+    signer: new InMemorySigner(process.env.NEXT_PUBLIC_PRIVATE_KEY!),
+  });
+
+  const contractAddress: string = 'KT1HTphtfX4mWv8BUCb6gL4FLZGMq6MeQTnw';
+
+  const connectWallet = async (): Promise<void> => {
+    try {
+      const available = await TempleWallet.isAvailable();
+      if (!available) {
+        throw new Error("Temple Wallet not installed");
+      }
+
+      const wallet: TempleWallet = new TempleWallet('MyAwesomeDapp');
+      await wallet.connect('ghostnet');
+      const tezosInstance = wallet.toTezos();
+      setTezos(tezosInstance);
+      const address: string = await tezosInstance.wallet.pkh();
+      setWalletAddress(address);
+      setConnected(true);
+    } catch (error: any) {
+      console.error('Error connecting wallet:', error);
+    }
+  };
+
+  const getBalance = async (): Promise<void> => {
+    try {
+      if (!tezos) {
+        throw new Error('Tezos not initialized');
+      }
+      const userWallet = await tezos.wallet.pkh();
+      const contract = await tezos.wallet.at(contractAddress);
+      const result = await contract.contractViews.get_balance_of([{owner: userWallet, token_id: 0}]).executeView({
+        source: userWallet,
+        viewCaller: contractAddress
+      });
+      console.log(result[0].balance.c[0]);
+      if (result) {
+        setBalance(result[0].balance.c[0]);
+      }
+    } catch (error: any) {
+      console.error('Error getting balance:', error);
+    }
+  };
+
+  const mintToken = async (): Promise<void> => {
+    try {
+      if (!Tezos || !tezos) {
+        throw new Error('Tezos not initialized');
+      }
+      const contract = await Tezos.contract.at(contractAddress); // Use Tezos for contract initialization
+      const mintOne = 1;
+
+      console.log(`Minting ${mintOne} crystal...`);
+      const operation = await contract?.methodsObject.mint([{ to_: await tezos.wallet.pkh(), amount: mintOne }])?.send();
+
+      console.log(`Waiting for ${operation?.hash} to be confirmed...`);
+      await operation?.confirmation(1);
+
+      console.log(`Operation injected: https://testnet.tzstats.com/${operation?.hash}`);
+      window.alert(`Mint operation successful! https://testnet.tzstats.com/${operation?.hash}`);
+      await getBalance(); // Update balance after minting
+    } catch (error: any) {
+      console.error('Error minting tokens:', error);
+      window.alert('Error minting tokens: ' + error.message);
+    }
+  };
+
+  const burnToken = async (): Promise<void> => {
+    try {
+      if (!tezos) {
+        throw new Error('Tezos not initialized');
+      }
+      const contract = await tezos.wallet.at(contractAddress); // Use Tezos for contract initialization
+      const ONE = 1;
+
+      console.log(`Burning ${ONE} crystal...`);
+      const operation = await contract.methodsObject.burn([{ from_: await tezos.wallet.pkh(), token_id: 0, amount: ONE }]).send();
+
+      console.log(`Waiting for ${operation?.opHash} to be confirmed...`);
+      await operation?.confirmation(1);
+
+      console.log(`Operation injected: https://testnet.tzstats.com/${operation?.opHash}`);
+      window.alert(`Burn operation successful! https://testnet.tzstats.com/${operation?.opHash}`);
+      await getBalance(); // Update balance after minting
+    } catch (error: any) {
+      console.error('Error burning tokens:', error);
+      window.alert('Error burning tokens: ' + error.message);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container mx-auto mt-5">
+      <h1 className="text-3xl font-semibold mb-5">Tezos Smart Contract Interaction</h1>
+      {!connected && (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3" onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      )}
+      {connected && (
+        <div>
+          <p>Connected Wallet Address: {walletAddress}</p>
+          {balance !== undefined && <p>Balance: {balance}</p>}
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-3" onClick={mintToken}>
+            Mint Token
+          </button>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-3 ml-3" onClick={burnToken}>
+            Burn Token
+          </button>
+          <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-3 ml-3" onClick={getBalance}>
+            Get Balance
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      )}
+    </div>
   );
-}
+};
+
+export default TezosContract;
